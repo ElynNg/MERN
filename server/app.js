3bc4b57bf3125f5b3fec1dc87fa8eea5059f1977
Elyn Nguyen <ndym1489@gmail.com>
2025-01-13 18:17:56 +0900

    add postRoute

--- server/app.js
+++ server/app.js
@@ -1,7 +1,35 @@
 import express from "express";
+import mongoose from "mongoose";
+import config from "./config";
+import hpp from "hpp";
+import helmet from "helmet";
+import cors from "cors";
+import morgan from "morgan";
+//Routes
+import postsRoutes from "./routes/api/post";
 
 const app = express();
+const { MONGO_URI } = config;
+
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, credentials: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+mongoose
+    .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully connect to MongoDB"))
+    .catch((e) => console.log(e));
+
+//Use routes
 
 app.get("/");
+app.use("/api/post", postsRoutes);
 
 export default app;
---
+++ server/routes/api/post.js
@@ -0,0 +1,33 @@
+import express from "express";
+
+//Model
+import Post from "../../models/post";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+    const postFindResult = await Post.find();
+
+    console.log(postFindResult, "All post get!");
+
+    res.json(postFindResult);
+});
+
+router.post("/", async (req, res) => {
+    try {
+        console.log(req, "req");
+        const { title, contents, fileUrl, creator } = req.body;
+        const newPost = await Post.create({
+            title,
+            contents,
+            fileUrl,
+            creator,
+        });
+
+        res.json(newPost);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+export default router;
